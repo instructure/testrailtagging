b39630852a5488caabca01be6622ec57da4adf50
Santosh Natarajan <snatarajan@instructure.com>
2016-06-28 14:27:12 -0600

    handle api rate limit/throttle
    Change-Id: I948283aa95afec6b9cfff3bca01b23224fa9fc70

--- lib/files/testrail_apiclient_retry.rb
+++ lib/files/testrail_apiclient_retry.rb
@@ -12,6 +12,12 @@ module TestRail
       # Gurock api's often deadlocks with errors like this:
       #   TestRail API returned HTTP 500 ("Deadlock found when trying to get lock; try restarting transaction")
       # So if they say to retry, then that's what we will do
+
+      # Please note that the API is rate limited on TestRail Hosted and may throttle requests.
+      # TestRail might also return a 429 Too Many Requests response which you are expected to handle.
+      # Such a response also includes a Retry-After header indicating how many seconds to wait
+      # before you are allowed to submit the next request.
+      # http://docs.gurock.com/testrail-api2/introduction #Rate Limit
       response = nil
       3.times do
         begin
@@ -20,6 +26,10 @@ module TestRail
         rescue TestRail::APIError => e
           if e.message && e.message.match("HTTP 500.*Deadlock")
             sleep 1
+          elsif e.message && e.message.match("HTTP 429")
+            retry_seconds = response['Retry-After']
+            puts "TestRail rate limited. retrying in #{retry_seconds}"
+            sleep retry_seconds
           else
             # Don't retry it, let the exception propagate
             raise
